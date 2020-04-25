d812b90f72353ac1d355d51e04c45831be05e4f1
supernoir <kawohl.bentodesign@gmail.com>
2020-04-25 19:31:25 +0200

    adds article component, dbdump

---
+++ src/components/Article/template.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { Paper } from "../Paper"
+import { Section } from "../Section"
+import { Paragraph } from "grommet"
+
+export interface ArticleProps {
+  title: string,
+  children: React.ReactNode
+}
+
+export const Article: React.FC<ArticleProps> = ({ title, children }) => {
+  return (
+    <Paper>
+      <Section>
+        <h1>{title}</h1>
+        <Paragraph>
+          {children}
+        </Paragraph>
+      </Section>
+
+    </Paper>
+  )
+
+}
--- src/pages/public/About/template.tsx
+++ src/pages/public/About/template.tsx
@@ -1,23 +1,18 @@
 import React from 'react'
 import { RouteComponentProps } from '@reach/router'
-import { Paragraph } from './styles'
-import { Paper } from '../../../components/Paper'
-import { Section } from '../../../components/Section'
+import { Article } from '../../../components/Article'
 
 interface AboutPageProps extends RouteComponentProps { }
 
 export const AboutPage: React.FC<AboutPageProps> = (props) => {
-  return (<Paper>
-    <Section>
-      <h1>{"About the project"}</h1>
-      <Paragraph>
-        On March 22, 2020 in the midst of a global pandemic, a doctor wrote a New York Times OpEd with a simple request:
-        “Big tech needs to rapidly build and scale a cloud-based national ventilator surveillance platform which … would allow hospitals nationwide to report their I.C.U. bed status and their ventilator supply daily, in an unprecedented data-sharing initiative.”
-        We are a group of volunteers here to answer the call to action. We built this platform with the mission to:
-        provide an aggregated overview of critical ventilator demand.
-        connect medical facilities with verified ventilator suppliers.
-      </Paragraph>
-    </Section>
-
-  </Paper>)
+  return (<>
+    <Article
+      title={"About the project"}>
+      On March 22, 2020 in the midst of a global pandemic, a doctor wrote a New York Times OpEd with a simple request:
+      “Big tech needs to rapidly build and scale a cloud-based national ventilator surveillance platform which … would allow hospitals nationwide to report their I.C.U. bed status and their ventilator supply daily, in an unprecedented data-sharing initiative.”
+      We are a group of volunteers here to answer the call to action. We built this platform with the mission to:
+      provide an aggregated overview of critical ventilator demand.
+      connect medical facilities with verified ventilator suppliers.
+        </Article>
+  </>)
 }
