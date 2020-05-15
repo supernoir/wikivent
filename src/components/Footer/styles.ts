ccf7c939964237540afb50e59ec640fa1395e348
supernoir <kawohl.bentodesign@gmail.com>
2020-05-15 17:33:12 +0200

    updates and refactors article type pages

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,6 +1,17 @@
 import styled from "styled-components"
+import { Link } from "@reach/router"
 
 export const StyledFooter = styled.footer`
   margin: 1em 0;
   color: snow;
+`
+
+export const FooterLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  cursor: pointer;
+
+  &:hover {
+  text-decoration: underline;
+  }
 `
